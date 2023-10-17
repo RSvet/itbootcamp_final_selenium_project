f3b946b03b768e692d01a9ed8bd3558bac2ae90f
Svetlana Radisic <svetlana_radisic@yahoo.com>
2023-10-17 14:42:47 +0200

    Added test for profile route when user is not authenticated

--- src/test/java/tests/AuthRoutesTests.java
+++ src/test/java/tests/AuthRoutesTests.java
@@ -12,4 +12,12 @@ public class AuthRoutesTests extends BasicTest{
                 .withMessage("Url should be "+baseUrl+"/login!")
                 .until(ExpectedConditions.urlToBe(baseUrl+"/login"));
     }
+    @Test(retryAnalyzer = RetryAnalyzer.class)
+    public void forbidsVisitsToProfileUrlIfNotAuthenticated(){
+        driver.navigate().to(baseUrl+"/profile");
+        wait
+                .withMessage("Url should be "+baseUrl+"/login!")
+                .until(ExpectedConditions.urlToBe(baseUrl+"/login"));
+    }
+
 }
